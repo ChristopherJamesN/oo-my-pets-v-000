d59cf3a7052c31a0796fe38319d91083b248a30e
Chris Nady <nady.christopher@gmail.com>
2017-09-11 22:02:20 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,17 +30,17 @@ class Owner
   end
 
   def buy_fish(name)
-    new_fish = New.fish(name)
+    new_fish = Fish.new(name)
     @pets[:fishes].push(new_fish)
   end
 
   def buy_cat(name)
-    new_cat = New.cat(name)
+    new_cat = Cat.new(name)
     @pets[:cats].push(new_cat)
   end
 
   def buy_dog(name)
-    new_dog = New.dog(name)
+    new_dog = Dog.new(name)
     @pets[:dogs].push(new_dog)
   end
 
